4104d7335bc927d039f559ef1fd4a24f4f17dc5c
adam <iammanyadams@gmail.com>
2020-03-12 19:52:05 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,5 @@
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :name, :artist_name, :song_name
   @@all = []
 
   def self.all
@@ -9,5 +9,66 @@ class Song
   def save
     self.class.all << self
   end
+
+
+	def self.create
+		song = Song.new
+		song.save
+		song
+	end
+
+
+
+	def self.new_by_name(song_name)
+		song = self.new
+		song.name = song_name
+		song
+	end
+
+	def self.create_by_name(song_name)
+		song = self.new
+		song.name = song_name
+		@@all << song
+		song
+	end
+	def self.find_by_name(song_name) 
+		self.all.detect {|song| song.name == song_name}
+	end
+
+	def self.find_or_create_by_name(song_name)
+		self.find_by_name(song_name) || self.create_by_name(song_name)
+	end
+	def self.alphabetical
+		self.all.sort_by{|song| song.name}
+	end
+
+
+	def self.new_from_filename(filename)
+		song = self.new
+		file_name = filename.delete_suffix('.mp3').split(' - ') 
+		song.artist_name = file_name[0] 
+		song.name = file_name[1]
+		
+		song
+end
+
+	def self.create_from_filename(filename)
+		song = self.new
+		file_name = filename.delete_suffix('.mp3').split(' - ') 
+		song.artist_name = file_name[0]
+		song.name = file_name[1]
+	self.all << song
+end
+
+
+
+
+
+ 	def self.destroy_all
+ 		@@all.map {|all| @@all = [] }
+ 	end
+
+
+
 
 end
